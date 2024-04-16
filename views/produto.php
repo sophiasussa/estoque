04acf5bd8c9e9eb4138b8be458df1f9c81bc36e7
sophiasussa <sophiasussa@outlook.com>
2024-04-16 17:24:32 -0300

    Criação produto.php

--- views/produto.php
+++ views/produto.php
@@ -16,26 +16,32 @@ if (isset($_SESSION['mensagem'])) {
     <div class="row">
         <div class="col">
             <div class="d-flex justify-content-between mb-3">
-                <h1 class="text-center mb-0">Lista de Categorias</h1>
-                <a href="?pg=form_categoria" class="btn btn-success" role="button">Cadastrar</a>
+                <h1 class="text-center mb-0">Lista de Produtos</h1>
+                <a href="?pg=form_produto" class="btn btn-success" role="button">Cadastrar</a>
             </div>
             <table class="table">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Categoria</th>
+                        <th>Preço</th>
                         <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php  foreach ($categorias as $categoria) : ?>
+                    <?php  foreach ($produtos as $produto) : ?>
                         <tr>
-                            <td><?php echo htmlspecialchars($categoria->getId()); ?></td>
-                            <td><?php echo htmlspecialchars($categoria->getNome()); ?></td>
+                            <td><?php echo htmlspecialchars($produto->getId()); ?></td>
+                            <td><?php echo htmlspecialchars($produto->getNome()); ?></td>
+                            <td><?php echo htmlspecialchars($produto->getDescricao()); ?></td>
+                            <td><?php echo htmlspecialchars($produto->getCategoria()); ?></td>
+                            <td><?php echo htmlspecialchars($produto->getPreco()); ?></td>
                             <td>
-                                <a class="" href="?pg=form_categoria&id=<?php echo $categoria->getId(); ?>">
+                                <a class="" href="?pg=form_produto&id=<?php echo $produto->getId(); ?>">
                                     <i class="fas fa-eye"></i></a>
-                                <a class="" href="?pg=delete_categoria&id=<?php echo $categoria->getId(); ?>" onclick="return confirm('Tem certeza que deseja excluir esta categoria?')">
+                                <a class="" href="?pg=delete_produto&id=<?php echo $produto->getId(); ?>" onclick="return confirm('Tem certeza que deseja excluir esta categoria?')">
                                     <i class="fas fa-trash-alt"></i></a>
                             </td>
                         </tr>
