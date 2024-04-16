d0fa5284c5331cceb2749f71e155e9f15ddab3c7
sophiasussa <sophiasussa@outlook.com>
2024-04-16 17:33:32 -0300

    Criação form_produto

---
+++ views/form_produto.php
@@ -0,0 +1,55 @@
+<?php
+// include_once("restrict.php");
+require_once "controllers/ProdutoController.php";
+// Inicia a sessão
+if (isset($_GET["id"])) {
+	$produtoController = new ProdutoController();
+	$produto = $produtoController->findById($_GET["id"]);
+}
+
+if (
+	isset($_POST["nome"])
+    isset($_POST["descricao"])
+    isset($_POST["categoria"])
+    isset($_POST["preco"])
+) {
+	$produtoController = new ProdutoController();
+
+	// Construindo o Produto
+	$produto = new Produto(null, $_POST["nome"], $_POST["descricao"], $_POST["categoria"], $_POST["preco"]);
+
+	// Salvando ou Atualizando Produto
+	if (isset($_GET["id"])) {
+		$produto->setId($_GET["id"]);
+		$produtoController->update($produto);
+	} else {
+
+		$produtoController->save($produto);
+	}
+
+	// Voltando pra tela anterior
+	header("Location: ?pg=produto");
+
+	// Encerra a execução do script php
+	exit();
+}
+
+?>
+
+<div class="container mt-2">
+	<h1 class="text-center mb-0">Cadastro de Produto</h1>
+	<form method="POST">
+
+		<div class="form-group">
+			<label for="nome">Nome</label>
+			<input type="text" class="form-control" id="nome" name="nome" value="<?php echo isset($produto) ? $produto->getNome() : ''; ?>">
+            <label for="descricao">Descrição</label>
+			<input type="text" class="form-control" id="descricao" name="descricao" value="<?php echo isset($produto) ? $produto->getDescricao() : ''; ?>">
+            <label for="categoria">Categoria</label>
+			<input type="text" class="form-control" id="categoria" name="categoria" value="<?php echo isset($produto) ? $produto->getCategoria() : ''; ?>">
+            <label for="preco">Preço</label>
+			<input type="text" class="form-control" id="preco" name="preco" value="<?php echo isset($produto) ? $produto->getPreco() : ''; ?>">
+		</div>
+		<input type="submit" class="btn btn-primary" id="salvar" name="salvar" value="Salvar">
+	</form>
+</div>
